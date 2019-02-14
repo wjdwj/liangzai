cadb92e3064c630d2dce8d2ac55baa2440675c13
huashuo <1411453711@qq.com>
2019-02-14 11:36:02 +0800

    xiugai

---
+++ vue2-elm/src/router/router.js
@@ -0,0 +1,147 @@
+import App from '../App'
+
+const home = r => require.ensure([], () => r(require('../page/home/home')), 'home')
+const city = r => require.ensure([], () => r(require('../page/city/city')), 'city')
+const login = r => require.ensure([], () => r(require('../page/login/login')), 'login')
+const msite = r => require.ensure([], () => r(require('../page/msite/msite')), 'msite')
+const search = r => require.ensure([], () => r(require('../page/search/search')), 'search')
+const food = r => require.ensure([], () => r(require('../page/food/food')), 'food')
+const shop = r => require.ensure([], () => r(require('../page/shop/shop')), 'shop')
+const foodDetail = r => require.ensure([], () => r(require('../page/shop/children/foodDetail')), 'foodDetail')
+const shopDetail = r => require.ensure([], () => r(require('../page/shop/children/shopDetail')), 'shopDetail')
+const shopSafe = r => require.ensure([], () => r(require('../page/shop/children/children/shopSafe')), 'shopSafe')
+const profile = r => require.ensure([], () => r(require('../page/profile/profile')), 'profile')
+const info = r => require.ensure([], () => r(require('../page/profile/children/info')), 'info')
+const setusername = r => require.ensure([], () => r(require('../page/profile/children/children/setusername')), 'setusername')
+const address = r => require.ensure([], () => r(require('../page/profile/children/children/address')), 'address')
+const add = r => require.ensure([], () => r(require('../page/profile/children/children/children/add')), 'add')
+const addDetail = r => require.ensure([], () => r(require('../page/profile/children/children/children/children/addDetail')), 'addDetail')
+const points = r => require.ensure([], () => r(require('../page/points/points')), 'points')
+const pointsDetail = r => require.ensure([], () => r(require('../page/points/children/detail')), 'pointsDetail')
+const confirmOrder = r => require.ensure([], () => r(require('../page/confirmOrder/confirmOrder')), 'confirmOrder')
+const remark = r => require.ensure([], () => r(require('../page/confirmOrder/children/remark')), 'remark')
+const payment = r => require.ensure([], () => r(require('../page/confirmOrder/children/payment')), 'payment')
+const userValidation = r => require.ensure([], () => r(require('../page/confirmOrder/children/userValidation')), 'userValidation')
+const invoice = r => require.ensure([], () => r(require('../page/confirmOrder/children/invoice')), 'invoice')
+const chooseAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/chooseAddress')), 'chooseAddress')
+const addAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/addAddress')), 'addAddress')
+const searchAddress = r => require.ensure([], () => r(require('../page/confirmOrder/children/children/children/searchAddress')), 'searchAddress')
+export default [{
+    path: '/',
+    component: App, //顶层路由，对应index.html
+    children: [ //二级路由。对应App.vue
+        //地址为空时跳转home页面
+        {
+            path: '',
+            redirect: '/home'
+        },
+        //首页城市列表页
+        {
+            path: '/home',
+            component: home
+        },
+          //登录注册页
+          {
+            path: '/login',
+            component: login
+        },
+        //当前选择城市页
+        {
+            path: '/city/:cityid',
+            component: city
+        },
+         //所有商铺列表页
+         {
+            path: '/msite',
+            component: msite,
+            meta: { keepAlive: true },
+        },  
+        {
+            path: '/food',
+            component: food
+        },
+        //搜索页
+        {
+            path: '/search/:geohash',
+            component: search
+        },
+          //商铺详情页
+          {
+            path: '/shop',
+            component: shop,
+            children: [{
+                path: 'foodDetail', //食品详情页
+                component: foodDetail,
+            }, {
+                path: 'shopDetail', //商铺详情页
+                component: shopDetail,
+                children: [{
+                    path: 'shopSafe', //商铺安全认证页
+                    component: shopSafe,
+                }, ]
+            }]
+        },
+          //确认订单页
+          {
+            path: '/confirmOrder',
+            component: confirmOrder,
+            children: [{
+                path: 'remark', //订单备注
+                component: remark,
+            }, {
+                path: 'invoice', //发票抬头
+                component: invoice,
+            }, {
+                path: 'payment', //付款页面
+                component: payment,
+            }, {
+                path: 'userValidation', //用户验证
+                component: userValidation,
+            }, {
+                path: 'chooseAddress', //选择地址
+                component: chooseAddress,
+                children: [{
+                    path: 'addAddress', //添加地址
+                    component: addAddress,
+                    children: [{
+                        path: 'searchAddress', //搜索地址
+                        component: searchAddress,
+                    }]
+                }, ]
+            }, ]
+        },
+        //个人信息页
+        {
+            path: '/profile',
+            component: profile,
+            children: [{
+                path: 'info', //个人信息详情页
+                component: info,
+                children: [{
+                    path: 'setusername',
+                    component: setusername,
+                },{
+                    path: 'address',
+                    component: address,     //编辑地址
+                    children:[{
+                        path:'add',
+                        component:add,
+                        children:[{
+                            path:'addDetail',
+                            component:addDetail
+                        }]
+                    }]
+                }]
+             }]
+        },
+        //我的积分页
+        {
+            path: 'points',
+            component: points,
+            children: [{
+                path: 'detail', //积分说明
+                component: pointsDetail,
+            }, ]
+        },
+    ]
+}]
